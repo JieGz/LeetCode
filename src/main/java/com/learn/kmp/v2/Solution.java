940aba942d847dd447348b7796deb77f6438a90f
Luke <gdjiegz@gmail.com>
2020-12-01 19:07:16 +0800

    feat: 更优雅一点点的解法,注意i是一定大于或者等于j的

---
+++ src/main/java/com/learn/kmp/v2/Solution.java
@@ -0,0 +1,34 @@
+package com.learn.kmp.v2;
+
+public class Solution {
+    public static void main(String[] args) {
+        String mainStr = "ABC ABCDABCD";
+        String m = "ABCDABCD";
+        System.out.println(new Solution().violentMatch(mainStr, m));
+    }
+
+
+    private int violentMatch(String s, String p) {
+        final char[] sChar = s.toCharArray();
+        final char[] pChar = p.toCharArray();
+        int index = -1;
+
+        int i = 0, j = 0;
+
+        while (i < sChar.length && j < pChar.length) {
+            if (sChar[i] == pChar[j]) {
+                ++i;
+                ++j;
+            } else {
+                i = i - j + 1;
+                j = 0;
+            }
+        }
+
+        if (j == pChar.length) {
+            index = i - j;
+        }
+
+        return index;
+    }
+}
