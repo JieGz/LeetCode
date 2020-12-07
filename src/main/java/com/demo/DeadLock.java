46f147ff2ec9f7eb59f31f1eee787f21e39a01d1
Luke <gdjiegz@gmail.com>
2020-12-07 12:07:46 +0800

    feat: 死锁demo

---
+++ src/main/java/com/demo/DeadLock.java
@@ -0,0 +1,52 @@
+package com.demo;
+
+import java.util.concurrent.TimeUnit;
+
+public class DeadLock {
+
+    private String lockA = "LockA";
+    private String lockB = "LockB";
+
+    /**
+     * 产生死锁的方法
+     */
+    private void deadLock() {
+        //构建一个线程t1,先拿lockA的锁,再拿lockB的锁
+        final Thread t1 = new Thread(() -> {
+            synchronized (lockA) {
+                sleep(2000);
+                synchronized (lockB) {
+                    //do something
+                }
+            }
+        });
+        //构建一个线程t2,先拿lockB的锁,再拿lockA的锁
+        final Thread t2 = new Thread(() -> {
+            synchronized (lockB) {
+                synchronized (lockA) {
+                    //do something
+                }
+            }
+        });
+        //分别启动t1,t2
+        t1.start();
+        t2.start();
+    }
+
+    /**
+     * 模拟业务,休眠一段时间
+     *
+     * @param ms 休眠时间
+     */
+    private void sleep(long ms) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new DeadLock().deadLock();
+    }
+}
